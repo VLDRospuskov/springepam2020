ddc62bb7ab77212b85c69ec245a14395d23fe8b7
Vladislav Rospuskov <Vladislav_Rospuskov@epam.com>
2020-07-15 17:42:16 +0300

    Update gitignore

--- src/main/java/com/epam/springepam2020/Application.java
+++ src/main/java/com/epam/springepam2020/Application.java
@@ -1,7 +1,6 @@
 package com.epam.springepam2020;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
